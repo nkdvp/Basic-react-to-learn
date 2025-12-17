f0fe2b519b9e932cd67fd7d785b878e001e0aec1
Neo <duc.kim.nguyen@codelink.io>
2025-12-17 14:43:23 +0700

    jump to feature

--- src/App.js
+++ src/App.js
@@ -54,13 +54,32 @@ export default function Game() {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
+    function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+
   return (
     <div className="game">
       <div className="game-board" >
-        <Board isX={isX} squares={currentSquares} onPlay={handlePlay}/>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol onClick>{moves}</ol>
       </div>
     </div>
   );
